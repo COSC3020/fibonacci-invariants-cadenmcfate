3f4fc86528e029268096ab7f66cd73d7616b4939
Caden McFate <cadenmcfate@gmail.com>
2024-02-02 22:13:23 -0700

    implimented fib function

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
 function fib(n) {
-    return n;
+    var fibArr = [];
+    for (var i = 0; i <= n; i++) {
+	if (i === 0) fibArr[i]=0;
+	else if (i === 1) fibArr[i]=1;
+	else fibArr[i] = fibArr[i-1] + fibArr[i-2];
+    }
+    return fibArr;
 }
